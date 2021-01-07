0dc4479c7b0f0e1e3c04f64c6d5539dc1bc2264e
da3m0nsec <alvifr@hotmail.com>
2021-01-07 05:55:53 -0500

    Add bilinear interpolation

--- imagevision/ImageProcessor.java
+++ imagevision/ImageProcessor.java
@@ -380,7 +380,7 @@ interface InterpolationMethods {
         return in.getPixel(Xnear,Ynear);
     }
 
-    public static int bilinearAdjust (ImageProcessor in, BufferedImage out, int x, int y) {
+        /*
         //esto me suena, está feo
         float gx = ((float) x) / out.getWidth() * (in.getWidth() -1);
         float gy = ((float) y) / out.getHeight() * (in.getHeight() -1);
@@ -398,7 +398,18 @@ interface InterpolationMethods {
         int p = x-gxi;
         int q = y-gyi;
 
-        grey = ImageProcessor.clamp(c+(d-c)*p+(a-c)*q+(b+c-a-d)*p*q,0,255);
+        //grey = ImageProcessor.clamp(c+(d-c)*p+(a-c)*q+(b+c-a-d)*p*q,0,255);
+        /*
+        float hi = (gxi+1-x)*a + (x-gxi)*b;
+        float hii = (gxi+1-x)*c + (x-gxi)*d;
+        float yi = (gyi+1-y)*hi + (y-gyi)*hii;
+        grey = ImageProcessor.clamp(yi, 0, 255);
+        
+        float hi = c*(1-x) + a*x;
+        float hii = d*(1-x) + b*x;
+        float yi = hi*(1-y) + hii*y;
+        grey = ImageProcessor.clamp(yi, 0, 255);
+
         /*
         for (int i=0; i<=2; ++i){
             float b00 = get(c00, i);
@@ -409,7 +420,45 @@ interface InterpolationMethods {
             int ble = ((int) blerp(b00,b10,b01,b11,gx-gxi,gy-gyi)) << (8*i);
             grey = grey | ble;
         }
-        */
+       
         return grey;
+        */
+        /* gets the 'n'th byte of a 4-byte integer */
+    private static int get(int self, int n) {
+        return (self >> (n * 8)) & 0xFF;
+    }
+ 
+    private static float lerp(float s, float e, float t) {
+        return s + (e - s) * t;
+    }
+ 
+    private static float blerp(final Float c00, float c10, float c01, float c11, float tx, float ty) {
+        return lerp(lerp(c00, c10, tx), lerp(c01, c11, tx), ty);
+    }
+ 
+    //private static BufferedImage scale(BufferedImage self, float scaleX, float scaleY) {
+    public static int bilinearAdjust (ImageProcessor self, BufferedImage newImage, int x, int y) {
+    
+        //BufferedImage newImage = new BufferedImage(newWidth, newHeight, self.getType());
+        float gx = ((float) x) / newImage.getWidth() * (self.getWidth() -1);
+        float gy = ((float) y) / newImage.getHeight() * (self.getHeight() -1);
+
+        int gxi = (int) gx; //????????????
+        int gyi = (int) gy; //????????????
+        int rgb = 0;
+        int c00 = self.getPixel(gxi, gyi);
+        int c10 = self.getPixel(gxi + 1, gyi);
+        int c01 = self.getPixel(gxi, gyi + 1);
+        int c11 = self.getPixel(gxi + 1, gyi + 1);
+        for (int i = 0; i <= 2; ++i) {
+            float b00 = get(c00, i);
+            float b10 = get(c10, i);
+            float b01 = get(c01, i);
+            float b11 = get(c11, i);
+            int ble = ((int) blerp(b00, b10, b01, b11, gx - gxi, gy - gyi)) << (8 * i);
+            rgb = rgb | ble;
+        }
+        
+        return rgb;
     }
 }
